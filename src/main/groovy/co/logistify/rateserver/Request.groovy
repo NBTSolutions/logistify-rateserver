f98b842a142dd033b6c7cd7fad5f3b1f48daa8d7
Travis Webb <travis@traviswebb.com>
2012-06-11 00:45:12 -0400

    added request validation switched to post request

--- src/main/groovy/co/logistify/rateserver/Request.groovy
+++ src/main/groovy/co/logistify/rateserver/Request.groovy
@@ -1,8 +1,13 @@
 package co.logistify.rateserver
 
 class Request {
-    String fromZip;
-    String toZip;
-    List<String> classes;
-    List<String> weights;
+    String fromZip
+    String toZip
+    List<String> classes
+    List<Integer> weights
+
+    String scac
+    String login
+    String pass
+    String acct
 }
--- src/test/groovy/co/logistify/rateserver/RateJsonTest.groovy
+++ src/test/groovy/co/logistify/rateserver/RateJsonTest.groovy
@@ -1,6 +1,5 @@
 package co.logistify.rateserver
 
-import co.logistify.rateserver.Rate
 import groovy.util.GroovyTestCase
 
 class RateJsonTest extends GroovyTestCase {
@@ -8,7 +7,7 @@ class RateJsonTest extends GroovyTestCase {
      * More than anything, this was a test to help me learn how JsonBuilder 
      * works.
      */
-    void testJsonOutput() {
+    void testGroovyToJson() {
         def rate = new Rate([
             netCharge   : 50,
             grossCharge : 100,
@@ -20,5 +19,35 @@ class RateJsonTest extends GroovyTestCase {
             ]) ]
         ])
         assert new groovy.json.JsonBuilder(rate).toString() == '{"note":"hello","lineItems":[{"description":"nothing","netCharge":20.0,"grossCharge":10.0}],"netCharge":50.0,"grossCharge":100.0}'
+    }
+
+    void testJsonToGroovy() {
+        def request = new Request([
+            fromZip : '23188',
+            toZip   : '24060',
+            classes : [
+                '50', '75'
+            ],
+            weights : [
+                1000, 2000
+            ],
+            scac    : 'CNWY'
+        ])
+        // convert from object to json string
+        String requestStr = new groovy.json.JsonBuilder(request).toString()
+
+        // convert string to Request object
+        Request r = new groovy.json.JsonSlurper().parseText(requestStr)
+
+        assert r.fromZip == '23188'
+        assert r.toZip == '24060'
+        assert r.classes == [ '50', '75' ]
+        assert r.weights == [ 1000, 2000 ]
+        assert r.login == null
+        assert r.pass == null
+        assert r.acct == null
+        assert r.scac == 'CNWY'
+
+        assert new groovy.json.JsonBuilder(r).toString() == requestStr
     }
 }
